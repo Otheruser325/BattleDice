b58a26c0a731b0ff95f74cdc994fd7be8a4f510d
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2026-05-23 01:56:24 +0100

    Fix Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: 'public',
-  server: {
-    host: '0.0.0.0',
-    port: 5173
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ["lucide-react"],
   },
+  base: "./",
   build: {
-    target: 'es2022'
-  }
+    outDir: "dist",
+  },
 });
