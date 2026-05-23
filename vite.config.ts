5eb72695c6c148f8f875a01941973f1dce0693a2
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2026-05-23 02:09:08 +0100

    Fix Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
@@ -10,5 +9,9 @@ export default defineConfig({
   base: "./",
   build: {
     outDir: "dist",
+  },
+  define: {
+    'import.meta.env.VITE_ENABLE_DEV_MENU': JSON.stringify(process.env.VITE_ENABLE_DEV_MENU || 'true'),
+    'import.meta.env.VITE_DEBUG_LOGS': JSON.stringify(process.env.VITE_DEBUG_LOGS || 'true'),
   },
 });
