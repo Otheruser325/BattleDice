bf2c5d952f831407618fc674967c76703a5991d1
Otheruser325 <73720030+Otheruser325@users.noreply.github.com>
2026-05-23 02:48:44 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
-  base: '/BattleDice/' || '/',
+  base: process.env.REPO_FIX || '/',
   build: {
     outDir: "dist",
   },
